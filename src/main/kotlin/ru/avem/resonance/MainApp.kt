d8c36f72d4c4debb8a52f3262b89bc5074626da7
it-HP <svz161@mail.ru>
2019-11-07 16:30:09 +0300

    Initial commit

---
+++ src/main/kotlin/ru/avem/resonance/MainApp.kt
@@ -0,0 +1,10 @@
+package ru.avem.resonance
+
+class MainApp {
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            println("Hello World!")
+        }
+    }
+}
